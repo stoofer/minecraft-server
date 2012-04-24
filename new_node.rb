b48c19609d7dbe4945b53a995f9d3f0671a2634d
Stuart Caborn <stooferus@gmail.com>
2012-04-24 16:55:57 +0100

    Generate node config

---
+++ new_node.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'json'
+
+node_config = {
+  "run_list" => ["role[ec2-minecraft-server]"],
+  "aws" => {
+    "aws_access_key_id" => ENV['AWS_ACCESS_KEY_ID'],
+    "aws_secret_access_key"=> ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
+File.open("nodes/#{ARGV[0]}.json", 'w'){|f| f.write node_config.to_json }
